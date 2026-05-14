79072b4997d534678702f50a84ce737b119454d9
Para-Finn <ro.turner0307@gmail.com>
2026-05-14 14:30:58 -0500

    Set up express server

--- ekovis-app/src/App.jsx
+++ ekovis-app/src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home.jsx';
 import Species from './Species.jsx';
+import UserProfile from './User_Profile.jsx';
 
 function App() {
   
   return(
     <BrowserRouter>
-        <h1>Test</h1>
         <nav>
-          <Link to="/">Home</Link> |{" "}
+          <Link to="/">Home</Link> | {" "}
+          <Link to="/profile">Profile</Link> | {" "}
           <Link to="/species">Playable Species</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/species" element={<Species />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
     </BrowserRouter>
   );
---
+++ ekovis-app/src/User_Profile.jsx
@@ -0,0 +1,9 @@
+function UserProfile() {
+    return (
+
+       <p>This is the user's profile</p>
+        
+    );
+}
+
+export default UserProfile;
